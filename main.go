4acc07346ae4319d9225a0c506d4c476e8c5405c
Kristoffer Berdal <web@flexd.net>
2018-03-07 17:55:06 +0100

    Look for config file in the working directory first, then /etc. Easier for testing

--- main.go
+++ main.go
@@ -16,8 +16,8 @@ var urlRegexp = regexp.MustCompile(`<([^>]*)\|([^>]*)>`)
 
 func main() {
 	viper.SetConfigName("config")    // name of config file (without extension)
-	viper.AddConfigPath("/etc/b2s/") // path to look for the config file in
 	viper.AddConfigPath(".")         // optionally look for config in the working directory
+	viper.AddConfigPath("/etc/b2s/") // path to look for the config file in
 	viper.SetConfigType("yaml")
 	err := viper.ReadInConfig() // Find and read the config file
 	if err != nil {             // Handle errors reading the config file
